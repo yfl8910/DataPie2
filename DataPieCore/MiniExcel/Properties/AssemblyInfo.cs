516b7f59fb6feaacfb92a4e8a85bdc993da28d6a
yfl8910 <yfl8910@qq.com>
2022-06-30 15:37:55 -0700

    update

--- DataPieCore/MiniExcel/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿#if DEBUG
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("MiniExcelTests")]
-#endif
-
-#if PLAT_SKIP_LOCALS_INIT
-[module: System.Runtime.CompilerServices.SkipLocalsInit]
-#endif
